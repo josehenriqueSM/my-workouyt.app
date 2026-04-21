0c9452541a9a20308378b298125df0f8f03c4b66
josehenriqueSM <jhenriquemacedo99@gmail.com>
2026-04-21 13:05:56 -0300

    Configuring NativeWind

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,11 @@
 import { Text, View } from "react-native";
-
-export default function Home() {
+ 
+export default function App() {
   return (
-    <View>
-      <Text>
-        Henrique
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
       </Text>
     </View>
-  )
+  );
 }
